8336d2e132685d12003c8568bbc752bda728cb25
sundarraj-usistech <102951821+sundarraj-usistech@users.noreply.github.com>
2022-04-04 13:49:47 +0530

    Create dojfilter.php

---
+++ dojfilter.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Search by Date of Joining</title>
+	</head>
+	<body>
+		<style type="text/css">
+			table,th,td{
+				border: 1px solid black;
+			}
+			td{
+				text-align: center;
+			}
+		</style>
+		<h4>Select the Date to Search</h4>
+		<form action="dojfilter.php" method="post">
+			<input type="date" name="emp_doj">
+			<button type="submit" name="submit">SEARCH</button>
+		</form>
+		<br>
+		<?php 
+			require 'dbconnect.php';
+			if(isset($_POST['submit'])){
+				$emp_doj=$_POST['emp_doj'];
+				$sql="SELECT * FROM employee_details WHERE employee_doj='$_POST[emp_doj]'";
+				$result=mysqli_query($conn,$sql);
+				$rowCount=mysqli_num_rows($result);
+				if ($rowCount>0) {
+					echo "<table><tr><th>Employee ID</th><th>Employee Name</th><th>Designation</th><th>E-Mail ID</th><th>Date of Joining</th><th>Mobile Number</th><th>Employee Status</th></tr>";
+					while ($data=mysqli_fetch_assoc($result)) {
+						echo "<tr><td> ".$data['employee_id']."</td><td> ".$data['employee_name']."</td><td>".$data['employee_designation']."</td><td>".$data['employee_mail_id']."</td><td>".$data['employee_doj']."</td><td>".$data['employee_phone']."</td><td>".$data['employee_status']."</td></tr>";
+					}
+					echo '</table>';	
+				}
+				
+				else{
+					echo "No data found";
+				}
+			}
+			mysqli_close($conn);
+		?>
+	</body>
+	</html>	
